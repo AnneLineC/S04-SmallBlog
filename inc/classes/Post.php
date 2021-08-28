f280aea97183ac04c666fd5fdce951ce11a13444
AnneLineC <anneline.col@gmail.com>
2021-08-28 12:04:33 +0200

    Création des Classes

---
+++ inc/classes/Post.php
@@ -0,0 +1,44 @@
+<?php
+
+class Post {
+    private $title;
+    private $date;
+    private $content;
+    private $category;
+    private $author;
+
+    public function __construct($title = '', $date = '', $content = '', $category = '', $author = '')
+    {
+        $this->title = $title;
+        $this->date = $date;
+        $this->content = $content;
+        $this->category = $category;
+        $this->author = $author;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+}
--- index.php
+++ index.php
@@ -13,11 +13,11 @@
 
 <div class="container">
     <header class="page-header">
-        <h1 class="main-title">Blog O'Poil</h1>
+        <h1 class="main-title"><a href="index.php">Blog O'Poil</a></h1>
         <p class="site-desc">Un petit blog tout épuré pour s'entraîner à utiliser PHP et MySQL</p>
         <nav class="site-nav">
             <ul class="site-nav__list">
-                <li class="site-nav__item"><a class="site-nav__link">Home</a></li>
+                <li class="site-nav__item"><a class="site-nav__link" href="index.php">Home</a></li>
                 <li class="site-nav__item"><a class="site-nav__link">Catégorie 1</a></li>
                 <li class="site-nav__item"><a class="site-nav__link">Catégorie 2</a></li>
                 <li class="site-nav__item"><a class="site-nav__link">Catégorie 3</a></li>
