5e01c8a447e0c44088e0ec2d27aae60d2cf1439e
AnneLineC <anneline.col@gmail.com>
2021-08-28 10:59:58 +0200

    Création base HTML et CSS

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog O'Poil</title>
+
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+    <header class="page-header">
+        <h1 class="main-title">Blog O'Poil</h1>
+        <p class="site-desc">Un petit blog tout épuré pour s'entraîner à utiliser PHP et MySQL</p>
+        <nav class="site-nav">
+            <ul class="site-nav__list">
+                <li class="site-nav__item"><a class="site-nav__link">Home</a></li>
+                <li class="site-nav__item"><a class="site-nav__link">Catégorie 1</a></li>
+                <li class="site-nav__item"><a class="site-nav__link">Catégorie 2</a></li>
+                <li class="site-nav__item"><a class="site-nav__link">Catégorie 3</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <main>
+        <article class="post">
+            <header class="post__header">
+                <h2 class="post__title">Titre de l'article</h2>
+                <p class="post__meta">Par <span class="post__author">Auteur</span> le jj mois année dans <span class="post__category">Catégorie</span></p>
+            </header>
+            <section class="post__content">
+                <p class="post__text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum pariatur hic rerum nemo explicabo quos cumque quo est iure blanditiis excepturi earum quidem labore similique tempore, ullam nisi autem vel dolorem consequuntur nobis fugiat delectus magni ut? Ipsa, quos? Quo error eaque est ipsam? Minima et incidunt ipsam inventore eum nisi dolores obcaecati possimus alias. Deleniti non ipsum magni beatae magnam voluptatibus, necessitatibus dicta accusamus blanditiis quam sequi? Excepturi incidunt commodi, aliquam quasi architecto qui, ad error a, voluptatem sint amet praesentium quas quod accusantium dolorem nobis. Molestiae, natus consequuntur amet modi excepturi, quibusdam minima doloremque exercitationem aut beatae quisquam corrupti non dolorem quidem officia quod. Corporis aliquam id repudiandae! Magnam nulla modi sed tenetur commodi placeat quidem qui dicta perspiciatis velit voluptas alias amet, molestias, iste enim. Laborum debitis obcaecati ipsum eveniet ratione nihil sapiente earum repudiandae. At illo neque saepe libero iure, excepturi odio placeat voluptates, nisi ipsum ex quas quaerat distinctio ullam sequi temporibus corporis facere molestias repudiandae, consequatur fugiat molestiae. Repudiandae corrupti quos nostrum iusto ratione? Vitae est totam eius veritatis optio cupiditate illum sit obcaecati magni? Autem sed dolorem iure ducimus, qui dolore? Temporibus sunt consequatur quibusdam. Fugit itaque, totam magni sint error nobis quam!
+                </p>
+            </section>
+        </article>
+
+        <article class="post">
+            <header class="post__header">
+                <h2 class="post__title">Titre de l'article</h2>
+                <p class="post__meta">Par <span class="post__author">Auteur</span> le jj mois année dans <span class="post__category">Catégorie</span></p>
+            </header>
+            <section class="post__content">
+                <p class="post__text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum pariatur hic rerum nemo explicabo quos cumque quo est iure blanditiis excepturi earum quidem labore similique tempore, ullam nisi autem vel dolorem consequuntur nobis fugiat delectus magni ut? Ipsa, quos? Quo error eaque est ipsam? Minima et incidunt ipsam inventore eum nisi dolores obcaecati possimus alias. Deleniti non ipsum magni beatae magnam voluptatibus, necessitatibus dicta accusamus blanditiis quam sequi? Excepturi incidunt commodi, aliquam quasi architecto qui, ad error a, voluptatem sint amet praesentium quas quod accusantium dolorem nobis. Molestiae, natus consequuntur amet modi excepturi, quibusdam minima doloremque exercitationem aut beatae quisquam corrupti non dolorem quidem officia quod. Corporis aliquam id repudiandae! Magnam nulla modi sed tenetur commodi placeat quidem qui dicta perspiciatis velit voluptas alias amet, molestias, iste enim. Laborum debitis obcaecati ipsum eveniet ratione nihil sapiente earum repudiandae. At illo neque saepe libero iure, excepturi odio placeat voluptates, nisi ipsum ex quas quaerat distinctio ullam sequi temporibus corporis facere molestias repudiandae, consequatur fugiat molestiae. Repudiandae corrupti quos nostrum iusto ratione? Vitae est totam eius veritatis optio cupiditate illum sit obcaecati magni? Autem sed dolorem iure ducimus, qui dolore? Temporibus sunt consequatur quibusdam. Fugit itaque, totam magni sint error nobis quam!
+                </p>
+            </section>
+        </article>
+    </main>
+    
+    <footer>
+    
+    </footer>
+</div>
+    
+</body>
+</html>
