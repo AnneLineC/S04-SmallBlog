e81382c4f4117c85b48215e738ab65c57455e21d
AnneLineC <anneline.col@gmail.com>
2021-08-28 12:25:59 +0200

    Mise en place du point d'entrée unique sur l'index via méthode GET

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
 
+    // ===============================================================
+    // Récupération de la page à afficher
+    // ===============================================================
+
+    // Par défaut, on considère qu'on affichera la page d'accueil
+    $pageToDisplay = 'home';
+
+    // Mais si un paramètre 'page' est présent dans l'URL, c'est
+    // qu'on veut afficher une autre page
+    if (!empty($_GET['page'])) {
+        $pageToDisplay = $_GET['page'];
+    }
+
+
+
+    // ===============================================================
+    // Affichage
+    // ===============================================================
+
     require 'inc/templates/header.tpl.php';
-
-    require 'inc/templates/home.tpl.php';
-
+    require 'inc/templates/' . $pageToDisplay . '.tpl.php';
     require 'inc/templates/footer.tpl.php';
 
 ?>
