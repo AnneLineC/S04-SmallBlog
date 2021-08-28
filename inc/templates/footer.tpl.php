b251e376ece1a56950ab8be7a50ecc6d6f6b4e57
AnneLineC <anneline.col@gmail.com>
2021-08-28 12:13:34 +0200

    Découpage de l'index en templates header, footer et home

---
+++ inc/templates/footer.tpl.php
@@ -0,0 +1,9 @@
+    </main>
+    
+    <footer class="site-footer">
+        <p class="site-footer__text">Un petit blog épuré par Anne-Line Colombo fin août 2021</p>
+    </footer>
+</div>
+    
+</body>
+</html>
---
+++ inc/templates/header.tpl.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog O'Poil</title>
+
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="container">
+    <header class="page-header">
+        <h1 class="main-title"><a href="index.php">Blog O'Poil</a></h1>
+        <p class="site-desc">Un petit blog tout épuré pour s'entraîner à utiliser PHP et MySQL</p>
+        <nav class="site-nav">
+            <ul class="site-nav__list">
+                <li class="site-nav__item"><a class="site-nav__link" href="index.php">Home</a></li>
+                <li class="site-nav__item"><a class="site-nav__link">Catégorie 1</a></li>
+                <li class="site-nav__item"><a class="site-nav__link">Catégorie 2</a></li>
+                <li class="site-nav__item"><a class="site-nav__link">Catégorie 3</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <main>
